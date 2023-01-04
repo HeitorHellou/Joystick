74ee76bb498ccd28145f58ab7aff49b6ef025698
Heitor Hellou <heitor.hellou@outlook.com>
2023-01-04 12:10:24 -0300

    d-pad

--- joystick/joystick.ino
+++ joystick/joystick.ino
@@ -10,6 +10,8 @@ const int ADC_Max = 1023;  // 10 bit
 // Set up the analogic pins
 const int Pin_LeftJoyX = A1;
 const int Pin_LeftJoyY = A0;
+const int Pin_RightJoyX = A3;
+const int Pin_RightJoyY = A2;
 
 // Button Pins
 // Set up the digital pins
@@ -18,12 +20,23 @@ const int Pin_ButtonB = 3;
 const int Pin_ButtonX = 4;
 const int Pin_ButtonY = 5;
 
+// Directional Pad Pins
+const int Pin_DpadUp    = 10;
+const int Pin_DpadDown  = 11;
+const int Pin_DpadLeft  = 12;
+const int Pin_DpadRight = 13;
+
 void setup() {
   // Set buttons as inputs, using internal pull-up resistors
 	pinMode(Pin_ButtonA, INPUT_PULLUP);
 	pinMode(Pin_ButtonB, INPUT_PULLUP);
 	pinMode(Pin_ButtonX, INPUT_PULLUP);
 	pinMode(Pin_ButtonY, INPUT_PULLUP);
+
+  pinMode(Pin_DpadUp, INPUT_PULLUP);
+	pinMode(Pin_DpadDown, INPUT_PULLUP);
+	pinMode(Pin_DpadLeft, INPUT_PULLUP);
+	pinMode(Pin_DpadRight, INPUT_PULLUP);
 
   XInput.setJoystickRange(0, ADC_Max);
 	XInput.setAutoSend(false);
@@ -37,12 +50,20 @@ void loop() {
 	boolean buttonX = digitalRead(Pin_ButtonX);
 	boolean buttonY = digitalRead(Pin_ButtonY);
 
+  boolean dpadUp = digitalRead(Pin_DpadUp);
+	boolean dpadDown = digitalRead(Pin_DpadDown);
+	boolean dpadLeft = digitalRead(Pin_DpadLeft);
+	boolean dpadRight = digitalRead(Pin_DpadRight);
+
   // Set XInput buttons
   // Convert the input into the Xbox respective
 	XInput.setButton(BUTTON_A, buttonA);
 	XInput.setButton(BUTTON_B, buttonB);
 	XInput.setButton(BUTTON_X, buttonX);
 	XInput.setButton(BUTTON_Y, buttonY);
+
+  // Set XInput DPAD values
+	XInput.setDpad(dpadUp, dpadDown, dpadLeft, dpadRight);
 
   // Set left joystick
 	if (UseLeftJoystick == true) 
@@ -54,6 +75,17 @@ void loop() {
 
 		XInput.setJoystickX(JOY_LEFT, leftJoyX);
 		XInput.setJoystickY(JOY_LEFT, leftJoyY, invert);
+	}
+
+  // Set right joystick
+	if (UseRightJoystick == true) {
+		int rightJoyX = analogRead(Pin_RightJoyX);
+		int rightJoyY = analogRead(Pin_RightJoyY);
+
+		boolean invert = !InvertRightYAxis;
+
+		XInput.setJoystickX(JOY_RIGHT, rightJoyX);
+		XInput.setJoystickY(JOY_RIGHT, rightJoyY, invert);
 	}
 
 	XInput.send();
